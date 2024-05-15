db58baf413be600c7a14c5a3fe63398a5219390b
christianzhou1 <christianzhiruizhou@outlook.com>
2024-05-15 15:02:16 -0300

    add scroll button, add media rule for menu

---
+++ src/scroll.js
@@ -0,0 +1,55 @@
+var scrollTopButton = document.getElementById("scroll-top-btn");
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollTopButton.style.display = "block";
+  } else {
+    scrollTopButton.style.display = "none";
+  }
+}
+// Bind your button click, scroll direction and effect speed
+document.getElementById("scroll-top-btn").onclick = function() {
+  scrollTo(0, 1000);
+
+}
+
+// Element to move, time in ms to animate
+function scrollTo(element, duration) {
+  var e = document.documentElement;
+  if (e.scrollTop === 0) {
+    var t = e.scrollTop;
+    ++e.scrollTop;
+    e = t + 1 === e.scrollTop-- ? e : document.body;
+  }
+  scrollToC(e, e.scrollTop, element, duration);
+}
+
+// Element to move, element or px from, element or px to, time in ms to animate
+function scrollToC(element, from, to, duration) {
+  if (duration <= 0) return;
+  if (typeof from === "object") from = from.offsetTop;
+  if (typeof to === "object") to = to.offsetTop;
+
+  scrollToX(element, from, to, 0, (1 / duration) * 2, 70, easeOutCuaic);
+}
+
+function scrollToX(element, xFrom, xTo, t01, speed, step, motion) {
+  if (t01 < 0 || t01 > 1 || speed <= 0) {
+    element.scrollTop = xTo;
+    return;
+  }
+  element.scrollTop = xFrom - (xFrom - xTo) * motion(t01);
+  t01 += speed * step;
+  debugger;
+  setTimeout(function() {
+    scrollToX(element, xFrom, xTo, t01, speed, step, motion);
+  }, step);
+}
+
+function easeOutCuaic(t) {
+  t--;
+  return t * t * t + 1;
+}
